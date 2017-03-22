d94be9b758bdeeb51eb033b118b18aa40c46add0
Jesus <chuy8jay@gmail.com>
2017-03-21 23:51:43 -0600

    Updated misra-gries

--- MisraGries/misra-gries.py
+++ MisraGries/misra-gries.py
@@ -1,7 +1,7 @@
+import random
 
-
-path = '/Users/jesuszarate/SchoolSemesters/Spring2017/CS6140-DataMining/FrequentItems/S1.txt'
-#path = '/Users/jesuszarate/SchoolSemesters/Spring2017/CS6140-DataMining/FrequentItems/S2.txt'
+#path = '/Users/jesuszarate/SchoolSemesters/Spring2017/CS6140-DataMining/FrequentItems/S1.txt'
+path = '/Users/jesuszarate/SchoolSemesters/Spring2017/CS6140-DataMining/FrequentItems/S2.txt'
 
 #a = [7,2,8,7,7,2,7,4,4,4,7]
 a = []
@@ -18,6 +18,8 @@ d = {}
 length = k - 1
 
 sub = 0
+
+
 
 def subtractFromAll():
     for l, c in d.items():
@@ -41,4 +43,12 @@ print d
 
 for l, c in d.items():
     print l + " , " + str(float(c)/1000000.0),
+print
 
+count = 0
+for c in a:
+    if c == 'a':
+        count += 1
+
+
+print "count = " + str(count)
